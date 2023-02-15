a4aa3ee5408cfa57c5da075d046deb8949cff33d
infiduk <infi_duk@naver.com>
2023-02-15 23:44:37 +0900

    :tada: Proudct: Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import GlobalStyles from 'styles/GlobalStyles';
+import { css } from "styled-components/macro"; //eslint-disable-line
+
+import SaaSProductLandingPage from "demos/SaaSProductLandingPage.js";
+import TermsOfServicePage from "pages/TermsOfService.js";
+import PrivacyPolicyPage from "pages/PrivacyPolicy.js";
+import ContractUs from "pages/ContractUs";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Router>
+        <Routes>
+        <Route path="/contact" element={<ContractUs />} />
+          <Route path="/privacy" element={<PrivacyPolicyPage />} />
+          <Route path="/terms" element={<TermsOfServicePage />} />
+          <Route path="/" element={<SaaSProductLandingPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
